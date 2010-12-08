10d5dd7a6e4fe8db95f02952ac7147add7108700
greg <greg@propdata.net>
2010-12-08 13:43:08 +0000

    Neaten up setting file for future sites

--- package_website_template/settings.py
+++ package_website_template/settings.py
@@ -5,24 +5,22 @@ DEV_MODE = True
 
 if DEV_MODE:
     DEBUG = True
-else:
-    DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 CURRENT_DIRECTORY = path.abspath(path.dirname(__file__))
 
 ### Site specifics ###
-SERVER_EMAIL = 'root@ws01.umh.propdata.net'
-WEB_STATS  = "http://ws01.umh.propdata.net/cgi-bin/awstats.pl?config=abcrealestate.co.za"
-WEBSITE_URL = "http://www.abcrealestate.co.za"
+SERVER_EMAIL = 'unconfigured@propdata.net'
+WEB_STATS    = "http://ws01.umh.propdata.net/cgi-bin/awstats.pl?config=unconfigured.co.za"
+WEBSITE_URL  = "http://www.unconfigured.co.za"
 
 # Brochure colours - Format: [RED, GREEN, BLUE]
-BROCHURE_TITLE_P1 = [0.82, 0.55, 0.0] # Top heading - first part
-BROCHURE_TITLE_P2 = [0.0, 0.0, 0.0] # Top heading - second part
-BROCHURE_DATE     = [0.80, 0.80, 0.80] # Date
-BROCHURE_HEADINGS = [0.0, 0.0, 0.0] # Section headings
-BROCHURE_PRICE    = [0.0, 0.0, 0.0] # Price
-BROCHURE_WEBSITE  = [0.82, 0.55, 0.0] # Website address (bottom)
+BROCHURE_TITLE_P1 = [0.82, 0.55, 0.0]   # Top heading - first part
+BROCHURE_TITLE_P2 = [0.0, 0.0, 0.0]     # Top heading - second part
+BROCHURE_DATE     = [0.80, 0.80, 0.80]  # Date
+BROCHURE_HEADINGS = [0.0, 0.0, 0.0]     # Section headings
+BROCHURE_PRICE    = [0.0, 0.0, 0.0]     # Price
+BROCHURE_WEBSITE  = [0.82, 0.55, 0.0]   # Website address (bottom)
 
 # This controls the clients add-ons
 COMMERCIAL_ADDON = True
@@ -33,7 +31,7 @@ BRANCH_LEVEL_ACCESS_ADDON = True
 ### Database details ###
 if DEV_MODE:
     DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    DATABASE_NAME = 'abcrealestate_emile'               # Or path to database file if using sqlite3.
+    DATABASE_NAME = 'unconfigured'          # Or path to database file if using sqlite3.
     DATABASE_USER = 'root'                  # Not used with sqlite3.
     DATABASE_PASSWORD = ''                  # Not used with sqlite3.
     DATABASE_HOST = '192.168.0.10'          # Set to empty string for localhost. Not used with sqlite3.
@@ -41,26 +39,25 @@ if DEV_MODE:
     GMAPS_KEY  = "ABQIAAAAkzGFPn2aXs-JhTpG4AIIORRpp2cC2FsRFUfKRDdI-3JIXl2s9BRxtJbBjImxjaIlIespam7WOi-8ag" # localhost
 else:
     DATABASE_ENGINE = 'postgresql_psycopg2' # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    DATABASE_NAME = 'abcrealestate'               # Or path to database file if using sqlite3.
-    DATABASE_USER = 'abcrealestate'               # Not used with sqlite3.
-    DATABASE_PASSWORD = 'kkuyvYUVUk8'       # Not used with sqlite3.
+    DATABASE_NAME = 'unconfigured'          # Or path to database file if using sqlite3.
+    DATABASE_USER = 'unconfigured'          # Not used with sqlite3.
+    DATABASE_PASSWORD = 'unconfigured'      # Not used with sqlite3.
     DATABASE_HOST = '196.37.205.41'         # Set to empty string for localhost. Not used with sqlite3.
     DATABASE_PORT = ''                      # Set to empty string for default. Not used with sqlite3.
-    GMAPS_KEY  = "ABQIAAAA_Yybjn6ImVf5RW9whzzNDBSC4RLQvT_DwTveTaBlF3030U__UxR1s8ntjiTom_p4WrEp8IichGFxVw" # abcrealestate.co.za
+    GMAPS_KEY  = "unconfigured" # unconfigured.co.za
 
 TIME_ZONE = 'Africa/Johannesburg'
 SECRET_KEY = 'w2ny#2xq&6yn)ffow!%(g@ww2r)mi03^(f+un-8_0$h#i6xlv5'
 
-if not DEV_MODE:
-    ANALYTICS_ID = 'UA-13148074-26'
-    MIDDLEWARE_CLASSES += (
-        'eos.lib.analytics.GoogleAnalyticsMiddleware',
-    )
+#if not DEV_MODE:
+#    ANALYTICS_ID = 'UA-13148074-26'
+#    MIDDLEWARE_CLASSES += (
+#        'eos.lib.analytics.GoogleAnalyticsMiddleware',
+#    )
 
 MEDIA_ROOT = '%s/assets' % CURRENT_DIRECTORY
-ROOT_URLCONF = 'abcrealestate.urls'
+ROOT_URLCONF = 'unconfigured.urls'
 
 TEMPLATE_DIRS += (
     '%s/html' % CURRENT_DIRECTORY,
 )
-
