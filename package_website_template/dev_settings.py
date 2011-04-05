50a51ad3e9debeabe8e970ceefda4f4a7b9760b2
greg <greg@propdata.net>
2011-04-05 15:24:02 +0000

    Add dev_settings.py to every project

---
+++ package_website_template/dev_settings.py
@@ -0,0 +1,10 @@
+from settings import *
+
+DEBUG = True
+
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'database_name'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = ''
+DATABASE_HOST = '192.168.0.10'
+DATABASE_PORT = ''
