27a33b59db35011ee58f6e40a0b10c993e769824
Jonathan Myburgh <jonathan@propdata.net>
2016-02-18 13:15:09 +0200

    Added variable

--- cloud_front_demo/manage.py
+++ cloud_front_demo/manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cloud_front_demo.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PROPDATA_WEBSITE_BUILDER_PROJECT_SETTINGS_MODULE_PATH.settings")
 
     from django.core.management import execute_from_command_line
 
