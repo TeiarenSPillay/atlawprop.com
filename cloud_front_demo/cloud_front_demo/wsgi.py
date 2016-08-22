e223d3deddcc1b77f539e53b5064781655719093
Jonathan Myburgh <jonathan@propdata.net>
2016-08-22 03:05:56 -0400

    Updated variable

--- cloud_front_demo/cloud_front_demo/wsgi.py
+++ cloud_front_demo/cloud_front_demo/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imagine_property_investments.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PROJECT_DEPLOYMENT_PROJECT_NAME.settings")
 
 application = get_wsgi_application()
